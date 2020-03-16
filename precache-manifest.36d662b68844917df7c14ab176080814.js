28b1f4780b06a212e576a6efa9ccf8ccbe51763c
Aluko Ayodele <51384137+Alukoayodele@users.noreply.github.com>
2020-03-16 15:56:56 +0100

    Updates

---
+++ precache-manifest.36d662b68844917df7c14ab176080814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce7cf08d2a72be8ecf44bca7ed476ba",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "d6395edbe1417e96ba25",
+    "url": "/first-app/static/css/main.a5ef70b6.chunk.css"
+  },
+  {
+    "revision": "73a68c16b6fcf71e9d9a",
+    "url": "/first-app/static/js/2.6a74901d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/first-app/static/js/2.6a74901d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6395edbe1417e96ba25",
+    "url": "/first-app/static/js/main.66c6c769.chunk.js"
+  },
+  {
+    "revision": "9cb14711f45955447fd3",
+    "url": "/first-app/static/js/runtime-main.9b2671df.js"
+  }
+]);
